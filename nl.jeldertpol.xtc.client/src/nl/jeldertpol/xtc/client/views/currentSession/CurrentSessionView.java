f3449da544ee6c6b98b8e3016164a2d0cba7386d
Jeldert Pol <software@jeldertpol.nl>
2008-08-20 14:47:05 +0000

    Open editor when doubleclicking, or pressing enter on resource in CurrentSessionView.

--- nl.jeldertpol.xtc.client/src/nl/jeldertpol/xtc/client/views/currentSession/CurrentSessionView.java
+++ nl.jeldertpol.xtc.client/src/nl/jeldertpol/xtc/client/views/currentSession/CurrentSessionView.java
@@ -6,13 +6,29 @@ import nl.jeldertpol.xtc.client.Activator;
 import nl.jeldertpol.xtc.client.session.whosWhere.WhosWhere;
 import nl.jeldertpol.xtc.client.session.whosWhere.WhosWhereListener;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
  * Display nicknames of clients and the file they are editing.
@@ -58,6 +74,7 @@ public class CurrentSessionView extends ViewPart implements WhosWhereListener {
 			column.setText(titles[i]);
 		}
 
+		// Fill table with current information
 		WhosWhere whosWhere = Activator.SESSION.getWhosWhere();
 		Set<String> nicknames = whosWhere.getNicknames();
 		for (String nickname : nicknames) {
@@ -71,6 +88,46 @@ public class CurrentSessionView extends ViewPart implements WhosWhereListener {
 		}
 
 		whosWhere.addListener(this);
+
+		final TableCursor cursor = new TableCursor(table, SWT.NULL);
+		cursor.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseUp(final MouseEvent e) {
+				// Nothing to do
+			}
+
+			@Override
+			public void mouseDown(final MouseEvent e) {
+				// Nothing to do
+			}
+
+			@Override
+			public void mouseDoubleClick(final MouseEvent e) {
+				int column = cursor.getColumn();
+				String text = cursor.getRow().getText(column);
+
+				handleEvent(column, text);
+			}
+		});
+		cursor.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(final KeyEvent e) {
+				// Enter
+				if (e.character == SWT.CR) {
+					int column = cursor.getColumn();
+					String text = cursor.getRow().getText(column);
+
+					handleEvent(column, text);
+				}
+			}
+
+			@Override
+			public void keyPressed(final KeyEvent e) {
+				// Nothing to do
+			}
+		});
 	}
 
 	/*
@@ -94,6 +151,50 @@ public class CurrentSessionView extends ViewPart implements WhosWhereListener {
 	public void updateWhosWhere(final String nickname, final String filePath) {
 		// Job will update UI.
 		new CurrentSessionUpdateJob(table, nickname, filePath);
+	}
+
+	private void handleEvent(final int column, final String text) {
+		if (column == COLUMN_NICKNAME) {
+			// TODO initiate chat
+			System.out.println("Chat: " + text);
+		} else if (column == COLUMN_RESOURCE) {
+			// Open resource
+			System.out.println("Open: " + text);
+
+			IProject project = ResourcesPlugin.getWorkspace().getRoot()
+					.getProject(Activator.SESSION.getCurrentProject());
+
+			IResource resource = project.findMember(text);
+			System.out.println("" + resource);
+			ITextEditor editor = Activator.documentReplacer
+					.findEditor(resource);
+
+			if (editor != null) {
+				editor.getEditorSite().getPage().activate(editor);
+			} else {
+				IEditorDescriptor desc = PlatformUI.getWorkbench()
+						.getEditorRegistry().getDefaultEditor(
+								resource.getName());
+
+				IWorkbenchPage page = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage();
+
+				IFile file = (IFile) resource;
+
+				try {
+					String editorID = EditorsUI.DEFAULT_TEXT_EDITOR_ID;
+					if (desc != null) {
+						editorID = desc.getId();
+					}
+
+					page.openEditor(new FileEditorInput(file), editorID);
+
+				} catch (PartInitException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
