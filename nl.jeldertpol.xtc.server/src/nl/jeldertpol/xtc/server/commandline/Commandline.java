df535baed91e5f8cfd9c4504c7b9157ef8185946
Jeldert Pol <software@jeldertpol.nl>
2008-09-09 12:39:31 +0000

    Kick a client.

--- nl.jeldertpol.xtc.server/src/nl/jeldertpol/xtc/server/commandline/Commandline.java
+++ nl.jeldertpol.xtc.server/src/nl/jeldertpol/xtc/server/commandline/Commandline.java
@@ -5,27 +5,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import nl.jeldertpol.xtc.common.conversion.Conversion;
 import nl.jeldertpol.xtc.common.session.SimpleSession;
 import nl.jeldertpol.xtc.server.Server;
-import aterm.ATerm;
-import aterm.ATermAppl;
-import aterm.ATermBlob;
+import toolbus.adapter.java.AbstractJavaTool;
 
 /**
- * @author Jeldert Pol
+ * Command Line Interface for the server. Can print current sessions and
+ * clients. Can also kick a client, in case the client crashed, but is still
+ * present on the server.
  * 
+ * @author Jeldert Pol
  */
 public class Commandline {
 	final Server server;
 
+	/**
+	 * Starting point for XTC Server. Can be called directly from the Toolbus
+	 * script. This supplies the correct args (as required by
+	 * {@link AbstractJavaTool#connect(String[])}).
+	 * 
+	 * @param args
+	 *            Arguments to connect to the Toolbus.
+	 * @throws Exception
+	 *             Thrown when something goes wrong during the parsing of the
+	 *             arguments or the establishing of the connection.
+	 */
 	public static void main(final String[] args) throws Exception {
 		new Commandline(args);
 	}
 
 	/**
-	 * @throws Exception
+	 * Constructor. Connects to the server, prints help, and starts taking user
+	 * input.
 	 * 
+	 * @param args
+	 *            Arguments to connect to the Toolbus.
+	 * @throws Exception
+	 *             Thrown when something goes wrong during the parsing of the
+	 *             arguments or the establishing of the connection.
 	 */
 	public Commandline(final String[] args) throws Exception {
 		super();
@@ -37,6 +54,9 @@ public class Commandline {
 		loopInput();
 	}
 
+	/**
+	 * Read the input on the command line, and process it.
+	 */
 	private void loopInput() {
 
 		while (true) {
@@ -47,31 +67,16 @@ public class Commandline {
 			} else if (input.equals("S")) {
 				showSessions();
 			} else if (input.equals("K")) {
-				// kickClient();
+				kickClient();
 			}
 		}
 	}
 
-	private void showSessions() {
-		List<SimpleSession> sessions = server.getSimpleSessions();
-
-		for (SimpleSession simpleSession : sessions) {
-			System.out.println(simpleSession.getProjectName() + " (" + simpleSession.getRevision() + ")");
-			for (String client : simpleSession.getClients()) {
-				System.out.println("- " + client);
-			}
-			System.out.println();
-		}
-		
-	}
-
-	private void printHelp() {
-		System.out.println("XTC administrative console.");
-		System.out.println("Usage:");
-		System.out.println("S: Show sessions and clients");
-		System.out.println("K: Kick a client");
-	}
-
+	/**
+	 * Read the input on the command line
+	 * 
+	 * @return The input, or an empty String ("") in case of an error.
+	 */
 	private String readInput() {
 		// open up standard input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -86,6 +91,84 @@ public class Commandline {
 		}
 
 		return input;
+	}
+
+	/**
+	 * Prints a help message.
+	 */
+	private void printHelp() {
+		System.out.println("XTC administrative console.");
+		System.out.println("Usage:");
+		System.out.println("S: Show sessions and clients.");
+		System.out
+				.println("K: Kick a client. (Should only be used if client crashed!)");
+	}
+
+	/**
+	 * Shows the sessions and clients on the server.
+	 */
+	private void showSessions() {
+		List<SimpleSession> sessions = server.getSimpleSessions();
+
+		for (SimpleSession simpleSession : sessions) {
+			System.out.println(simpleSession.getProjectName() + " ("
+					+ simpleSession.getRevision() + ")");
+			for (String client : simpleSession.getClients()) {
+				System.out.println("- " + client);
+			}
+			System.out.println();
+		}
+
+	}
+
+	/**
+	 * Kicks a client from the server. Asks for a session and client. Incorrect
+	 * input will be ignored.
+	 */
+	private void kickClient() {
+		System.out.println("Enter name of session:");
+		String inputProject = readInput();
+
+		List<SimpleSession> sessions = server.getSimpleSessions();
+
+		for (SimpleSession simpleSession : sessions) {
+			System.out.println(simpleSession.getProjectName());
+		}
+
+		boolean foundSession = false;
+		boolean foundClient = false;
+
+		for (SimpleSession simpleSession : sessions) {
+			if (simpleSession.getProjectName().equals(inputProject)) {
+				foundSession = true;
+
+				for (String client : simpleSession.getClients()) {
+					System.out.println("- " + client);
+				}
+
+				System.out.println("Enter name of client to kick:");
+				String inputClient = readInput();
+				for (String client : simpleSession.getClients()) {
+					if (client.equals(inputClient)) {
+						foundClient = true;
+
+						server.leaveSession(inputProject, inputClient);
+						System.out.println("Client kicked!");
+
+						break;
+					}
+				}
+				break;
+			}
+		}
+
+		if (!foundSession) {
+			System.out.println("Session not found.");
+		} else if (!foundClient) {
+			System.out.println("Client not found.");
+		}
+
+		printHelp();
 	}
 
 }
